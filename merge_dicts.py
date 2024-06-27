a362cea8d7c02a27ca3c7a0e018bd10c32b47e07
bsml320 <peilin.jia@gmail.com>
2024-06-27 19:46:38 +0800

    Add files via upload

---
+++ merge_dicts.py
@@ -0,0 +1,31 @@
+import os, re, sys
+import numpy as np
+import pandas as pd
+
+def merge_dicts(*dict_args):
+    result = {}
+    for item in dict_args:
+        result.update(item)
+
+    return result
+
+def merge_background_scores(filepath, identifier):
+    all_dict_background = []
+    for n in range(5):
+        dict_background = np.load(filepath + 'dict_%s_background_%s.npy' % (identifier, str(n)), allow_pickle=True).item()
+        all_dict_background.append(dict_background)   
+         
+    all_dict_background1 =  merge_dicts(*all_dict_background)    
+    
+    return all_dict_background1
+
+def merge_background_scores_K(filepath, identifier):
+    num = 9 if identifier == '100K' else 43
+    all_dict_background = []
+    for n in range(num):
+        dict_background = np.load(filepath + 'dict_score_background_%s_%s.npy' % (identifier, str(n)), allow_pickle=True).item()
+        all_dict_background.append(dict_background)   
+         
+    all_dict_background1 =  merge_dicts(*all_dict_background)    
+    
+    return all_dict_background1
